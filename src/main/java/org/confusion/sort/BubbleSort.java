73223543757a38f4811cac448a4c70fe54a880fc
Mohit Gupta <mohitkumargupta92@gmail.com>
2022-03-20 13:43:21 +0530

    Add Bubble Sort

---
+++ src/main/java/org/confusion/sort/BubbleSort.java
@@ -0,0 +1,46 @@
+package org.confusion.sort;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+// similar to the air bubble
+// larger bubble will rise first
+// in this largest element find its correct place in first iteration and so on
+// Best Case: O(N)
+// Worst Case: O(N*N)
+public class BubbleSort {
+
+    public static void main(String[] args) {
+        int[] array = new int[]{10, 5, 2, 3, 4, 9, 7, 8, 1, 6};
+//        int[] array = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        String iterationResult = Arrays.stream(array).boxed()
+                .map(element -> Integer.toString(element))
+                .collect(Collectors.joining(", "));
+        System.out.println(String.format("Input; Array Status: %s", iterationResult));
+        Arrays.stream(bubbleSort(array)).boxed().forEach(element -> System.out.println(element));
+    }
+
+    public static int[] bubbleSort(int array[]) {
+        for (int i = 0; i < array.length - 1; i++) {
+            boolean swappingRequired = false;
+            for (int j = 0; j < array.length - 1 - i; j++) {
+                if (array[j] > array[j + 1]) {
+                    swappingRequired = true;
+                    int temp = array[j + 1];
+                    array[j + 1] = array[j];
+                    array[j] = temp;
+                }
+
+                String iterationResult = Arrays.stream(array).boxed()
+                        .map(element -> Integer.toString(element))
+                        .collect(Collectors.joining(", "));
+                System.out.println(String.format("Iteration -> i: %s, j: %s; Array Status: %s", i, j, iterationResult));
+            }
+            if (swappingRequired == false) {
+                System.out.println("Array is sorted, hence breaking out of loop");
+                break;
+            }
+        }
+        return array;
+    }
+}
--- src/main/java/org/confusion/sort/SelectionSort.java
+++ src/main/java/org/confusion/sort/SelectionSort.java
@@ -1,20 +1,26 @@
 package org.confusion.sort;
 
 import java.util.Arrays;
+import java.util.stream.Collectors;
 
 // finding right place for position 1, then position 2
 // find the right place for index 0, then 1 and so on
 // in first iteration min element is placed at its correct place and so on.
 // in (n-1)th iteration second last and last item are placed at correct position
+// time complexity is same for worst and best case
+// Time Complexity O(n*n)
 public class SelectionSort {
 
     public static void main(String[] args) {
         int[] array = new int[]{10, 5, 2, 3, 4, 9, 7, 8, 1, 6};
+        String iterationResult = Arrays.stream(array).boxed()
+                .map(element -> Integer.toString(element))
+                .collect(Collectors.joining(", "));
+        System.out.println(String.format("Input; Array Status: %s", iterationResult));
         Arrays.stream(selectionSort(array)).boxed().forEach(element -> System.out.println(element));
     }
 
     public static int[] selectionSort(int[] array) {
-
         for (int i = 0; i < array.length - 1; i++) {
             for (int j = i + 1; j < array.length; j++) {
                 if (array[i] > array[j]) {
@@ -23,6 +29,10 @@ public class SelectionSort {
                     array[j] = array[i];
                     array[i] = temp;
                 }
+                String iterationResult = Arrays.stream(array).boxed()
+                        .map(element -> Integer.toString(element))
+                        .collect(Collectors.joining(", "));
+                System.out.println(String.format("Iteration -> i: %s, j: %s; Array Status: %s", i, j, iterationResult));
             }
         }
         return array;
